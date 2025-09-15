2458698f196333b02688ec2e38a293c576931294
Nandini-55 <njkstars1212@gmail.com>
2025-09-15 09:49:01 +0530

    View here

--- Lists/search_ele.py
+++ Lists/search_ele.py
@@ -2,5 +2,7 @@ lst = list(map(int,input().split()))
 search=int(input())
 for i in range(len(lst)) :
     if lst[i]==search:
-        print("Index of",search,"is",i)
+        print(1)
         break
+    if(i==len(lst)-1):
+        print(0)
