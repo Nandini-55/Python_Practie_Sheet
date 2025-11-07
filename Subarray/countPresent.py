46d41b11fafa36042f02ac5ce2688fccd83a0ff3
Nandini-55 <njkstars1212@gmail.com>
2025-11-07 21:28:00 +0530

    View here

--- Subarray/countPresent.py
+++ Subarray/countPresent.py
@@ -1,4 +1,4 @@
 arr= list(map(int,input().split()))
 index = int(input())
 count = (index+1)*(len(arr)-index)
-print("Sum of all subarrays sum : ",sum)
+print("Sum of all subarrays sum using index ",index,": ",sum)
