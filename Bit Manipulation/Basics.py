34787159cee5cacf562e5420ed3e46cc3d18dff8
Nandini-55 <njkstars1212@gmail.com>
2025-11-10 09:58:21 +0530

    View here

---
+++ Bit Manipulation/Basics.py
@@ -0,0 +1,8 @@
+#Bit manipulation 
+#Operations :-
+# & - if both values are 1 then 1 else 0
+# or - if both values are 0 then 0 else 1 
+# xor - if both values are same the 0 else 1
+
+#Properties - 
+# o - if a and 1==0 then its even  else odd
